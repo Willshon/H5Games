181f3d45acdf422c675f074c389b5213ce6456c3
Willshon <wsh752166675@163.com>
2015-07-01 18:21:48 +0800

    Initialize

---
+++ games/heibai/play68.js
@@ -0,0 +1,22 @@
+﻿function play68_init() {
+	updateShare(0);
+}
+
+function updateShare(bestScore) {
+	imgUrl = 'http://127.0.0.1/games/heibai/logo.png';
+	var domains = ['http://127.0.0.1','http://127.0.0.1','http://127.0.0.1','http://127.0.0.1'];
+	var domain = domains[new Date().getTime()%4];
+	lineLink = 'http://127.0.0.1/games/heibai/';
+	descContent = "史上导致最多手指和眼珠抽筋的游戏（别踩白块儿）！";
+	updateShareScore(bestScore);
+	appid = '';
+}
+
+function updateShareScore(bestScore) {
+	if(bestScore > 0) {
+		shareTitle = "我在《别踩白块儿》成绩" + bestScore + "，手指抽筋啦！";
+	}
+	else{
+		shareTitle = "史上导致最多手指和眼珠抽筋的游戏（别踩白块儿）！";
+	}
+}
---
+++ games/qedfc/js/config.main.js
@@ -0,0 +1,2 @@
+dataForWeixin.appId = "wx8820cdf5db680ffa";
+dataForWeixin.url = "http://127.0.0.1/"+_con["num"]+"/";
